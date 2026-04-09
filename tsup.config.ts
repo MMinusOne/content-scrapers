05e6f8c1a1e7fe212cc7700a378840d9b08d697c
MMinusOne <michael.business.lb@proton.me>
2026-04-09 13:20:55 +0300

    changed tsconfig

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: { resolve: true },
   clean: true,
   sourcemap: true,
+  bundle: false,
+  splitting: false,
+  external: ["fsevents"],
+  loader: {
+    ".node": "empty",
+  },
 });
